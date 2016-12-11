d2533763dee196c2acee7602867122da1bd40ed7
Lemon <Lemon@LemonMint>
2016-12-11 15:33:42 -0200

    Pronto parte das Ward o/o/o/o/o/o/o/o/

--- src/teste/calculo/Calculos.java
+++ src/teste/calculo/Calculos.java
@@ -23,6 +23,13 @@ public class Calculos {
     public double crowdControlDealt;
     public double wardLossPlaced;
     public double wardWinPlaced;
+    public double contadorVitorias10, contadorVitorias9, contadorDerrotas10, contadorDerrotas9;
+    public double totalPartidas;
+    public double porcentagemVitoria, vitoria10, vitoria9;
+    public double porcentagemDerrota, derrota10, derrota9;
+    public double wardMaior, wardMenor;
+    public double posterioreMaiorVitoria, posterioreMaiorDerrota;
+    public double posterioreMenorVitoria, posterioreMenorDerrota;
 
     private double porcentagemVitorias(List<Game> listaGames, List<FellowPlayer> jogador) {
         double vitorias = 0.;
@@ -30,7 +37,10 @@ public class Calculos {
         double totalCrowdControl = 0.;
         double total = 0.;
         double total2 = 0;
-        double temp = 0;
+        double contadorVitoriaWard = 0, contadorDerrotaWard = 0;
+        double wardVitoriaPlaced = 0, wardDerrotaPlaced = 0;
+        double contadorVitoria10 = 0, contadorVitoria9 = 0;
+        double contadorDerrota10 = 0, contadorDerrota9 = 0;
         for (Game atual : listaGames) {
             if (atual.getSubType().matches("(RANKED_SOLO_5x5)|(NORMAL)|(RANKED_FLEX_SR)")) {
                 if (atual.getStats().getWardPlaced() != null) {
@@ -39,22 +49,47 @@ public class Calculos {
                     vitorias += atual.getStats().getWin() ? 1 : 0;
 
                     if (atual.getStats().getWin()) {
-                        wardWinPlaced++;
+                        contadorVitoriaWard++;
+                        wardVitoriaPlaced += atual.getStats().getWardPlaced();
+                        if (atual.getStats().getWardPlaced() >= 10) {
+                            contadorVitoria10++;
+                        } else {
+                            contadorVitoria9++;
+                        }
                     } else {
-                        wardLossPlaced++;
+                        contadorDerrotaWard++;
+                        wardDerrotaPlaced += atual.getStats().getWardPlaced();
+                        if (atual.getStats().getWardPlaced() >= 10) {
+                            contadorDerrota10++;
+                        } else {
+                            contadorDerrota9++;
+                        }
                     }
                     totalWards += atual.getStats().getWardPlaced();
                     if (atual.getStats().getTotalTimeCrowdControlDealt() != null) {
-                        
+
                         total2++;
                         totalCrowdControl += atual.getStats().getTotalTimeCrowdControlDealt();
                     }
                 }
             }
         }
+        contadorVitorias10 += contadorVitoria10;
+        contadorVitorias9 += contadorVitoria9;
+        contadorDerrotas10 += contadorDerrota10;
+        contadorDerrotas9 += contadorDerrota9;
         crowdControlDealt += (totalCrowdControl / total);
         wardPlaced += (totalWards / total);
-        System.out.println("WARDS              : " + totalWards / total);
+        wardWinPlaced += (wardVitoriaPlaced / contadorVitoriaWard);
+        wardLossPlaced += (wardDerrotaPlaced / contadorDerrotaWard);
+        System.out.println("----------------------Media de JOGOS------------------------");
+        System.out.println("VITORIA 10 : " + contadorVitoria10);
+        System.out.println("VITORIA 9 : " + contadorVitoria9);
+        System.out.println("DERROTA 10 : " + contadorDerrota10);
+        System.out.println("DERROTA 9 : " + contadorDerrota9);
+        System.out.println("WARD WIN           : " + wardVitoriaPlaced / contadorVitoriaWard);
+        System.out.println("WARD LOSS          : " + wardDerrotaPlaced / contadorDerrotaWard);
+        System.out.println("WARDS TOTAL        : " + totalWards / total);
         System.out.println("CONTROL            : " + totalCrowdControl / total);
         System.out.println("numero de vitorias : " + vitorias / total);
         return vitorias / total;
@@ -73,6 +108,8 @@ public class Calculos {
         double porcentagemTotalWards = 0;
         double porcentagemCrowdControl = 0;
         double porcentagemTotalCrowdControl = 0;
+        double porcentagemWardVitoria = 0, porcentagemWardDerrota = 0;
+        double porcentagemWardVitoriaTotal = 0, porcentagemWardDerrotaTotal = 0;
         double totalListGames = 0;
         for (Game gameAtual : listGames) {
             if (gameAtual.getSubType().matches("(RANKED_SOLO_5x5)|(NORMAL)|(RANKED_FLEX_SR)")) {
@@ -83,6 +120,8 @@ public class Calculos {
                 double porcentagemMedia = 0.;
                 double contador = 0;
                 wardPlaced = 0;
+                wardWinPlaced = 0;
+                wardLossPlaced = 0;
                 crowdControlDealt = 0;
                 System.out.println("-------------------------------------------------------------------------------");
                 for (FellowPlayer fellowPlayer : fellowPlayers) {
@@ -90,24 +129,79 @@ public class Calculos {
                     if (Objects.equals(fellowPlayer.getTeamId(), gameAtual.getTeamId())) {
                         contador++;
                         PlayerGames playerGames = get2.getPlayerGames(fellowPlayer.getSummonerId());
-                        porcentagemMedia += porcentagemVitorias(playerGames.getGames(),fellowPlayers);
+                        porcentagemMedia += porcentagemVitorias(playerGames.getGames(), fellowPlayers);
                     }
                 }
                 porcentagemCrowdControl += crowdControlDealt / contador;
                 porcentagemWards += wardPlaced / contador;
                 porcentagemGames += porcentagemMedia / contador;
-                System.out.println("PORCENTAGEM DO JOGO ATUAL : " + porcentagemMedia / contador);
-                System.out.println("PORCENTAGEM DE WARDS DO JOGO ATUAL : " + wardPlaced / contador);
-                System.out.println("PORCENTAGEM DE CROWDCRONTROL JOGO ATUAL: " + crowdControlDealt / contador);
+                porcentagemWardVitoria += wardWinPlaced / contador;
+                porcentagemWardDerrota += wardLossPlaced / contador;
+
+                System.out.println("SOMATORIO DOS JOGADORES              : " + porcentagemMedia / contador);
+                System.out.println("SOMATORIO DE WARDS DOS JOGADORES     : " + wardPlaced / contador);
+                System.out.println("SOMATORIO DE WARDS VITORIA           : " + wardWinPlaced / contador);
+                System.out.println("SOMATORIO DE WARDS DERROTA           : " + wardLossPlaced / contador);
+                System.out.println("SOMATORIO DE CROWDCRONTROL JOGODORES : " + crowdControlDealt / contador);
             }
         }
-        porcentagemTotalCrowdControl = porcentagemCrowdControl/totalListGames;
+        porcentagemWardVitoriaTotal = porcentagemWardVitoria / totalListGames;
+        porcentagemWardDerrotaTotal = porcentagemWardDerrota / totalListGames;
+        porcentagemTotalCrowdControl = porcentagemCrowdControl / totalListGames;
         porcentagemTotal = porcentagemGames / totalListGames;
         porcentagemTotalWards = porcentagemWards / totalListGames;
-        System.out.println("PORCENTAGEM TOTAL : " + porcentagemTotal);
-        System.out.println("PORCENTAGEM TOTAL WARDS: " + porcentagemTotalWards);
+        totalPartidas = contadorDerrotas10 + contadorDerrotas9 + contadorVitorias10 + contadorVitorias9;
+        System.out.println("TOTAL DE PARTIDAS ANALISADAS : " + totalPartidas);
+        System.out.println("CONTADOR VITORIA >10 : " + contadorVitorias10);
+        System.out.println("CONTADOR DERROTAS >10: " + contadorDerrotas10);
+        System.out.println("CONTADOR VITORIAS <9 : " + contadorVitorias9);
+        System.out.println("CONTADOR DERROTAS <9 : " + contadorDerrotas9);
+        System.out.println("PORCENTAGEM TOTAL            : " + porcentagemTotal);
+        System.out.println("PORCENTAGEM TOTAL WARDS      : " + porcentagemTotalWards);
+        System.out.println("PORCENTAGEM WARDS VITORIA    : " + porcentagemWardVitoriaTotal);
+        System.out.println("PORCENTAGEM WARDS DERROTA    : " + porcentagemWardDerrotaTotal);
         System.out.println("PORCENTAGEM CROWDCONTROLDEALT: " + porcentagemTotalCrowdControl);
         System.out.println("**********************************************************************************");
+        CalculoEstatistico();
+        CalcularProbabilidadeMaior();
+        CalcularProbabilidadeMenor();
         return null;
+    }
+
+    public void CalculoEstatistico() {
+
+        porcentagemVitoria = (contadorVitorias10 + contadorVitorias9) / totalPartidas;
+        porcentagemDerrota = (contadorDerrotas10 + contadorDerrotas9) / totalPartidas;
+        vitoria10 = contadorVitorias10 / (contadorVitorias10 + contadorVitorias9);
+        vitoria9 = contadorVitorias9 / (contadorVitorias10 + contadorVitorias9);
+        derrota10 = contadorDerrotas10 / (contadorDerrotas10 + contadorDerrotas9);
+        derrota9 = contadorDerrotas9 / (contadorDerrotas10 + contadorDerrotas9);
+        System.out.println("Porcentagem Vitoria: " + porcentagemVitoria);
+        System.out.println("Porcentagem Derrota: " + porcentagemDerrota);
+        System.out.println("Porcentagem Vitoria ward 10: " + vitoria10);
+        System.out.println("Porcentagem Vitoria ward 9: " + vitoria9);
+        System.out.println("Porcentagem Derrota ward 10: " + derrota10);
+        System.out.println("Porcentagem Derrota ward 9: " + derrota9);
+        System.out.println("------------------------------------------------------------------");
+    }
+
+    public void CalcularProbabilidadeMaior() {
+
+        wardMaior = (porcentagemVitoria * vitoria10) + (porcentagemDerrota * derrota10);
+        //wardMenor = (porcentagemVitoria*vitoria9) + (porcentagemDerrota*derrota9);
+        posterioreMaiorVitoria = (porcentagemVitoria * vitoria10) / wardMaior;
+        posterioreMaiorDerrota = (porcentagemDerrota * derrota10) / wardMaior;
+        System.out.println("wardMaior: " + wardMaior);
+        System.out.println("posterioreMaiorVitoria: " + posterioreMaiorVitoria);
+        System.out.println("posterioreMaiorDerrota: " + posterioreMaiorDerrota);
+    }
+
+    public void CalcularProbabilidadeMenor() {
+        wardMenor = (porcentagemVitoria * vitoria9) + (porcentagemDerrota * derrota9);
+        posterioreMenorVitoria = (porcentagemVitoria * vitoria9) / wardMenor;
+        posterioreMenorDerrota = (porcentagemDerrota * derrota9) / wardMaior;
+        System.out.println("wardMenor: " + wardMenor);
+        System.out.println("posterioreMenorVitoria: " + posterioreMenorVitoria);
+        System.out.println("posterioreMenorDerrota: " + posterioreMenorDerrota);
     }
 }
